51c9c0c24750df06e46fc604b357143f97503b92
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-11-04 17:26:12 +0300

    Добавлена реализация 10 задачи для 24 варианта

--- ЛР №1/10.sql
+++ ЛР №1/10.sql
@@ -1,2 +1,8 @@
 --10.Показать поля TaxType, TaxRate и Name из таблицы Sales.SalesTaxRate.
 -- Все значения поля TaxType, равные 2, заменить на NULL. Названия полей оставить без изменений.
+select 
+	"Sales"."SalesTaxRate"."TaxType" 
+	, "Sales"."SalesTaxRate"."TaxRate" 
+	, "Sales"."SalesTaxRate"."Name"
+	, nullif("Sales"."SalesTaxRate"."TaxType", 2)
+from "Sales"."SalesTaxRate";
