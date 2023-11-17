c850f78b4b35cbf839f090b224fd29c08501d72c
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-11-17 14:06:20 +0300

    Внесены правки для 4 скрипта. Добавлен отчёт для второй лабораторной работы.

--- ЛР №2/04.sql
+++ ЛР №2/04.sql
@@ -1,4 +1,7 @@
+--Выбрать те линейки товаров (ProductLine) из таблицы Production.Product, где максимальная цена больше 3000 (из выборки исключить пустые поля).
 select 
 "ProductLine"
 from "Production"."Product" p 
-where "ListPrice" > 3000 and "ProductLine" is not null; 
+where "ProductLine" is not null
+group by "ProductLine"
+having MAX("ListPrice") > 3000;
