75782380ab489e2ddf137c02092e1c67a6d38ccf
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-12-01 13:03:35 +0300

    Исправлен скрипт 3 в ЛР4

--- ЛР №4/03.sql
+++ ЛР №4/03.sql
@@ -1,29 +1,14 @@
 --3. Показать товары, для которых существует более 2 классов, в одном стиле (стиль и класс определен)
 -- (Таблица Production.Product). Показать поля Name, Style и Class.
-select  "Name"
-, "Style"
-, "Class"
-from "Production"."Product"
-where "Style" is NOT null
-and "Class" is NOT null 
-and "Name" in 
-	(
-	select  "Name"
-	from "Production"."Product"
-	where "Style" is NOT null
-	and "Class" is NOT null
-	);
-	
---Реализация после правки
 SELECT "Name", "Style", "Class"
 FROM "Production"."Product" p
 WHERE "Style" IS NOT NULL 
 AND "Class" IS NOT NULL
-AND "Name" IN 
-    (SELECT "Name"
+AND "Name" is not null 
+    and exists (
+    SELECT count("Class")
      FROM "Production"."Product" pr
      WHERE "Style" IS NOT NULL 
      AND "Class" IS NOT NULL
-     AND p."Style" = pr."Style" 
-     GROUP BY "Name"  
-     HAVING COUNT("Class") > 2)
+     AND p."Style" = pr."Style"
+     HAVING COUNT("Class") > 2);
