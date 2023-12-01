dde71193bbe9f0dc29f39e1aea51641bff80d5ef
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-12-01 10:20:00 +0300

    Корретировки для сркипта 2 внесены

--- ЛР №3/02.sql
+++ ЛР №3/02.sql
@@ -1,10 +1,12 @@
 --2 Показать список продуктов (поле Name), в котором указано, есть ли у продукта
 --рейтинг или нет, из таблиц Production.Product, Production.ProductRewiew, используя LEFT OUTER JOIN.
-select  p."Name" 
+select  p."Name"
+, coalesce (pr."ProductID", 'Нет рейтинга') as Рейтинг_продукта
+, coalesce (pr."ProductID" is not null, 'Есть рейтинг') as Рейтинг_продукта
 		/*case 
            when pr."ProductID" is not null then 'Есть рейтинг' 
            else 'Нет рейтинга' 
        end as Рейтинг_продукта*/ 
        from "Production"."Product" p
-       coalesce (pr."ProductID", 'Нет рейтинга') as Рейтинг_продукта
+       
 left outer join "Production"."ProductReview" pr ON p."ProductID" = pr."ProductID";
