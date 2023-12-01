6f1491d739ab4de683c09d5af0867afae71c87b5
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-12-01 10:12:33 +0300

    внесены правки скриптам 3,5

--- ЛР №4/03.sql
+++ ЛР №4/03.sql
@@ -13,3 +13,17 @@ and "Name" in
 	where "Style" is NOT null
 	and "Class" is NOT null
 	);
+	
+--Реализация после правки
+SELECT "Name", "Style", "Class"
+FROM "Production"."Product" p
+WHERE "Style" IS NOT NULL 
+AND "Class" IS NOT NULL
+AND "Name" IN 
+    (SELECT "Name"
+     FROM "Production"."Product" pr
+     WHERE "Style" IS NOT NULL 
+     AND "Class" IS NOT NULL
+     AND p."Style" = pr."Style" 
+     GROUP BY "Name"  
+     HAVING COUNT("Class") > 2)
--- ЛР №4/05.sql
+++ ЛР №4/05.sql
@@ -15,3 +15,21 @@ where
         where  
             p1."Style" = p2."Style"
     );
+   
+--Внесены корректировки после правки
+select 
+    "Name", 
+    "ListPrice",
+    "Style"
+from 
+    "Production"."Product" p1
+where "ListPrice" > 
+   (
+   		select distinct  
+            --AVG(nullif("ListPrice",0))
+            AVG(CASE WHEN "ListPrice" <> 0 THEN "ListPrice" ELSE NULL END)
+        from 
+            "Production"."Product" p2
+        where  
+            p1."Style" = p2."Style"
+   )
