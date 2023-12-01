8574523debd0794f1d5403b9d1b0400a33087f50
Дудкин Александр Владимирович <dudkinav@masterscada.ru>
2023-12-01 12:16:35 +0300

    Исправлен скрипт 02, с использованием coalesce

--- ЛР №3/02.sql
+++ ЛР №3/02.sql
@@ -1,12 +1,7 @@
 --2 Показать список продуктов (поле Name), в котором указано, есть ли у продукта
 --рейтинг или нет, из таблиц Production.Product, Production.ProductRewiew, используя LEFT OUTER JOIN.
 select  p."Name"
-, coalesce (pr."ProductID", 'Нет рейтинга') as Рейтинг_продукта
-, coalesce (pr."ProductID" is not null, 'Есть рейтинг') as Рейтинг_продукта
-		/*case 
-           when pr."ProductID" is not null then 'Есть рейтинг' 
-           else 'Нет рейтинга' 
-       end as Рейтинг_продукта*/ 
+, coalesce (cast(pr."ProductID" as VARCHAR (50)), 'Нет рейтинга') as Рейтинг_продукта
        from "Production"."Product" p
        
 left outer join "Production"."ProductReview" pr ON p."ProductID" = pr."ProductID";
